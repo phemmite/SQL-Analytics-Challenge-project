4cbecbd06e5259db03d144a2c707615ef6ea289c
Ogunjobi Samuel <Phemmite@gmail.com>
2025-12-11 13:19:30 +0100

    Update SQLQuery12.sql

--- SQLQuery12.sql
+++ SQLQuery12.sql
@@ -1,9 +1,11 @@
+/*this show the first 100*/
 SELECT TOP (1000) [customer_id]
       ,[customer_unique_id]
       ,[customer_zip_code_prefix]
       ,[customer_city]
       ,[customer_state]
   FROM [Olist_db].[dbo].[olist_customers_dataset]
+
 
 
 
